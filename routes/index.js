00eaf04fa473d73dd72bda65fc40ba965a37d674
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-05-18 01:36:11 -0500

    add things

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('pages/index', { title: 'Express' });
+	res.render('pages/index', { title: 'Home Page' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+	res.render('pages/about', { title: 'About' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+	res.render('pages/projects', { title: 'Projects' });
+});
+
+/* GET portfolio page. */
+router.get('/portfolio', function(req, res, next) {
+	res.render('pages/portfolio', { title: 'Portfolio' });
+});
+
+/* GET mailinglist page. */
+router.get('/mailinglist', function(req, res, next) {
+	res.render('pages/mailinglist', { title: 'Mailing List' });
 });
 
 module.exports = router;
